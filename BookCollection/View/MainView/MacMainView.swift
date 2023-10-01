fd582f90f4370c6356773039b9fa3049ec27f40a
Illia Horevoi <illia.horevoi@gmail.com>
2023-10-01 16:04:43 +0300

    Mac fixes

--- BookCollection/View/MainView/MacMainView.swift
+++ BookCollection/View/MainView/MacMainView.swift
@@ -2,17 +2,19 @@ import SwiftUI
 
 #if os(macOS)
 struct MacMainView: View {
+    @EnvironmentObject var selectedStateModel: SelectedViewState
+    
     var body: some View {
         NavigationSplitView {
             AuthorsList()
         } content: {
-            if let selectedAuthor = selectedStateModel.selectedAuthor {
-                AuthorsBooks(author: selectedAuthor)
+            if selectedStateModel.selectedAuthor != nil {
+                AuthorsBooks()
             } else {
                 ZStack { }
             }
         } detail: {
-            if let selectedBook = selectedStateModel.selectedBook {
+            if selectedStateModel.selectedBook != nil {
                 ZStack { }
             } else {
                 ZStack { }
